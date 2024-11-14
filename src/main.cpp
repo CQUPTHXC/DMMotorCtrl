5b721ba997d30d75585bf3024844bf8e41b7ace6
qingmeijiupiao <2368357055@qq.com>
2024-11-14 21:52:22 +0800

    添加注释

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,15 @@ M2006_P36 M2006_1(1);
 class DM_MIT_MOTOR{
   public:
     DM_MIT_MOTOR(int _MST_ID,int _CAN_ID):MST_ID(_MST_ID),CAN_ID(_CAN_ID){
-      //motor_map[_MST_ID]=this;
+      motor_map[_MST_ID]=this;
+      //添加回调函数,当收到对应地址CAN数据时，调用callback
+      add_user_can_func(_MST_ID,callback);
     }
 
   //protected:
+  //地址到电机的映射
   static std::map<int,DM_MIT_MOTOR*> motor_map;
+  //回调函数,更新电机数据
   static void callback(twai_message_t* can_message){
     if(motor_map.find(can_message->identifier)==motor_map.end()){
       return;
@@ -32,6 +36,7 @@ class DM_MIT_MOTOR{
 
   void send_MIT_pakage(float _p_des,float _v_des,float _Kp,float _Kd,float _t_ff){
 
+    //定义打包函数
     auto get_MIT_pakage=[&](uint8_t* arr,uint16_t p_des,uint16_t v_des,uint16_t Kp,uint16_t Kd,uint16_t t_ff){
       arr[0]=p_des>>8;
       arr[1]=p_des&0xFF;
@@ -43,14 +48,19 @@ class DM_MIT_MOTOR{
       arr[7]=t_ff&0xFF;
     };
     
-    twai_message_t can_message;
-    can_message.identifier=CAN_ID;
-    can_message.data_length_code=8;
+    twai_message_t can_message;//定义CAN数据对象
+    can_message.identifier=CAN_ID;//电机ID就是CAN地址
+    can_message.data_length_code=8;//数据长度为8字节
+    //打包
     get_MIT_pakage(can_message.data,_p_des*(1<<16),_v_des*(1<<12),_Kp*(1<<8),_Kd*(1<<8),_t_ff*(1<<12));
+    
+    //发送CAN数据包
     twai_transmit(&can_message,portMAX_DELAY);
   }
+
+  //使能，达妙MIT模式必须先使能才能控制
   void enable(){
-      twai_message_t can_message;
+    twai_message_t can_message;
     can_message.identifier=CAN_ID;
     can_message.data_length_code=8;
     for (size_t i = 0; i < 7; i++)
@@ -61,7 +71,8 @@ class DM_MIT_MOTOR{
     
     twai_transmit(&can_message,portMAX_DELAY);
   }
-  
+  //更新数据的回调
+  //uint8_t* arr :CAN数据包
   void update_date_callback(uint8_t* arr){
     ERR=arr[0]>>4;
     POS_raw=(arr[1]<<8)|arr[2];
@@ -70,18 +81,21 @@ class DM_MIT_MOTOR{
     MOS_temp=arr[6];
     T_Rotor=arr[7];
   }
+  //控制任务,没写完
   static void control_task(void *p){
     DM_MIT_MOTOR* motor=(DM_MIT_MOTOR*)p;
 
   }
-  int MST_ID;
-  int CAN_ID;
-  uint16_t POS_raw;
-  uint16_t VEL_raw;
-  uint16_t TORQUE_raw;
-  uint8_t MOS_temp;
-  uint8_t T_Rotor;
-  uint8_t ERR;
+
+  int MST_ID;//电机反馈数据ID
+  int CAN_ID;//电机控制数据ID
+  //原始数据
+  uint16_t POS_raw;//位置
+  uint16_t VEL_raw;//速度
+  uint16_t TORQUE_raw;//扭矩
+  uint8_t MOS_temp;//MOS温度
+  uint8_t T_Rotor;//电机温度
+  uint8_t ERR;//错误信息
 
 };
 DM_MIT_MOTOR gm6220(0,1);
@@ -95,7 +109,8 @@ void setup() {
   digitalWrite(4,HIGH);
   digitalWrite(5,HIGH);
   can_setup();
-  delay(2000);
+  delay(2000);//等待CAN初始化和电机启动
+  //使能
   gm6220.enable();
 
 }
