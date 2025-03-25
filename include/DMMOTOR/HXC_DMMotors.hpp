2e89c5130ac9c1595ded6187065ec56eb0141041
qingmeijiupiao <2368357055@qq.com>
2025-03-25 20:18:00 +0800

    update

--- include/DMMOTOR/HXC_DMMotors.hpp
+++ include/DMMOTOR/HXC_DMMotors.hpp
@@ -3,7 +3,7 @@
  * @LastEditors: qingmeijiupiao
  * @Description: 调好默认PID参数的DM电机封装类，继承HXC_DMCtrl
  * @author: qingmeijiupiao
- * @LastEditTime: 2025-03-25 19:26:52
+ * @LastEditTime: 2025-03-25 20:17:43
  */
 
 
@@ -16,13 +16,38 @@ namespace HXC{
 //达妙3519
 class DM3519 : public HXC_DMCtrl{
     DM3519(HXC_CAN*can,int MST_ID,int CAN_ID):HXC_DMCtrl(can,MST_ID,CAN_ID){
+        this->set_Pmax();
+        this->set_Vmax();
+        this->set_Tmax();
+        
         set_speed_pid(pid_param(0.001,0.002,0,1,1));
         set_location_pid(pid_param(0.1,0.1,0,2000,500));
     };
     // 复用基类的构造函数
     DM3519(HXC_CAN* can, int MST_ID, int CAN_ID, pid_param speed_pid, pid_param location_pid)
-    :HXC_DMCtrl(can, MST_ID, CAN_ID, speed_pid, location_pid) {}
+    :HXC_DMCtrl(can, MST_ID, CAN_ID, speed_pid, location_pid) {
+        this->set_Pmax();
+        this->set_Vmax();
+        this->set_Tmax();
+    }
 };
 
+class DM6220 : public HXC_DMCtrl{
+    DM6220(HXC_CAN*can,int MST_ID,int CAN_ID):HXC_DMCtrl(can,MST_ID,CAN_ID){
+        this->set_Pmax();
+        this->set_Vmax();
+        this->set_Tmax();
+        set_speed_pid(pid_param(0.001,0.0006,0,1,1));
+        set_location_pid(pid_param(0.047,0.092,0,50,500));
+    };
+    // 复用基类的构造函数
+    DM6220(HXC_CAN* can, int MST_ID, int CAN_ID, pid_param speed_pid, pid_param location_pid)
+    :HXC_DMCtrl(can, MST_ID, CAN_ID, speed_pid, location_pid) {
+        this->set_Pmax();
+        this->set_Vmax();
+        this->set_Tmax();
+    }
+
+};
 }
 #endif
