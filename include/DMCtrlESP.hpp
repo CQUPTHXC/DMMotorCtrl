63fdca9c895996ca2972ce7b377d170db11fcf62
qingmeijiupiao <2368357055@qq.com>
2025-05-27 16:04:59 +0800

    fix bug

--- include/DMCtrlESP.hpp
+++ include/DMCtrlESP.hpp
@@ -2,7 +2,7 @@
  * @LastEditors: qingmeijiupiao
  * @Description: 达妙电机控制基类
  * @Author: qingmeijiupiao
- * @LastEditTime: 2025-05-11 14:00:36
+ * @LastEditTime: 2025-05-27 16:04:20
  */
 #ifndef DMCtrlESP_HPP
 #define DMCtrlESP_HPP
@@ -312,7 +312,7 @@ uint16_t DMMotor::get_pos_raw() {
  * @param {bool} is_multi_circle 是否是多圈位置
  */
 float DMMotor::get_pos_rad(bool is_multi_circle){
-    float angle=2.f*(POS_raw-32767)*Pmax/65535.f;
+    float angle=2.f*(get_location()-32767)*Pmax/65535.f;
     if(!is_multi_circle){
         angle=std::fmod(angle,2*PI);//取模
         if(angle<0)angle+=2*PI;//如果小于0，补齐
@@ -414,18 +414,15 @@ void DMMotor::update_date_callback(uint8_t* arr) {
     constexpr int MAX_POSITION=(1<<16)-1;//最大位置
     constexpr int HALF_POSITION=MAX_POSITION/2;//半最大位置
     // 更新电机的多圈位置
-    int delta = 0;
-    if ((POS + MAX_POSITION - POS_raw) % MAX_POSITION < HALF_POSITION) {  // 正转
-        delta = POS - POS_raw;
-        if (delta < 0) {
-            delta += MAX_POSITION;
-        }
-    } else {  // 反转
-        delta = POS - POS_raw;
-        if (delta > 0) {
-            delta -= MAX_POSITION;
-        }
+    int delta = POS - POS_raw;
+    
+    // 处理位置回绕情况
+    if (delta > HALF_POSITION) {
+        delta -= MAX_POSITION + 1;  // 正向回绕
+    } else if (delta < -HALF_POSITION) {
+        delta += MAX_POSITION + 1;  // 负向回绕
     }
+    
     POS_raw = POS;
     location += delta;  // 更新多圈位置
     last_can_message_update_time = now_time_ms();  // 更新最后更新时间
